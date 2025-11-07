2f48b3bc4cb4a98d203ef2e6c6c6120cb4c3f2dd
Aus Gomez <codewithaus@gmail.com>
2025-11-07 12:11:10 -0600

    Adding TreeNode Entity to Tree Module

--- src/tree/tree.module.ts
+++ src/tree/tree.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TreeService } from './tree.service';
 import { TreeController } from './tree.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TreeNode } from './tree-node.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TreeNode])],
   controllers: [TreeController],
   providers: [TreeService],
 })
